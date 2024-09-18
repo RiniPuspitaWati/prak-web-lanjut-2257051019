5c2310f4ec3d84e256ae6cccff2185d76bdd19f3
Rini Puspita Wati <rinipuspita600@gmail.com>
2024-09-18 15:32:18 -0800

    pembaruan 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/profile', [ProfileController::class, 'profile']);
+Route::get('/profile/{nama}', [ProfileController::class, 'profile']);
